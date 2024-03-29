b10cca6900208182e34ddd964faa46949577ae9e
geovlunaa <lunacarlos.dev@gmail.com>
2024-03-29 09:27:09 -0500

    dashboard

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1 className="font-bold">Customers</h1>;
+};
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1 className="text-2xl font-bold ">Invoices</h1>;
+};
+
+export default Page;
